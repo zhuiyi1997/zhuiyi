7228633be83ef290e9d28029662f300e2bde1b25
ytc <1099843740@qq.com>
2016-10-02 15:51:31 +0800

    three

--- app/controllers/IndexController.class.php
+++ app/controllers/IndexController.class.php
@@ -8,10 +8,12 @@ class IndexController extends \Controller{
 	{
 		//Log::log('查询了前十所学校');
 
-		$model = new School();
-		$result = $model->getOne(['id'=>1]);
+		//$model = new School();
+		//$result = $model->getOne(['id'=>1]);
+		echo "<body style='background:#2f373a;font-family:Arial,Helvetica,sans-serif;font-size:80px;line-height:10em;text-align:center;color:#4e4e4e;min-width:920px;border-top:10px solid #0c0e0e;'>Hello,world。</body>";
 		
-		$this->render('index/index',['name'=>$result]);
+		
+		//$this->render('index/index',['name'=>$result]);
 	}
 }
 ?>
